8c997a45dcc475e077632393e2be40718d993fbc
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2023-11-02 20:39:18 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<queue>
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N; cin >> N;
+    vector<int> V(N), DP(N);
+    for (auto& i : V) cin >> i;
+    for (int i = 0; i < N; i++) {
+        DP[i] = V[i];
+        for (int j = 0; j < i; j++) {
+            if (V[i] > V[j]) {
+                DP[i] = max(DP[i], DP[j] + V[i]);
+            }
+        }
+    }
+    //for (auto& i : DP) cout << i << " ";
+
+    cout << *max_element(DP.begin(), DP.end());
+
+    return 0;
+}
