e4272a95286a5afc08c5dab170fa2109b7c5b765
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2023-12-05 03:23:11 +0900

    [level 2] Title: 짝지어 제거하기, Time: 23.69 ms, Memory: 47 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,17 @@
+function solution(s)
+{
+    let stack = [];
+    stack.push(s[0]);
+    for(let i = 1;i<s.length;i++){
+        if(stack[stack.length-1] === s[i]){
+            stack.pop();
+        }else{
+            stack.push(s[i]);
+        }
+    }
+
+    if(stack.length > 0) return 0;
+    else return 1;
+    
+    return answer;
+}
