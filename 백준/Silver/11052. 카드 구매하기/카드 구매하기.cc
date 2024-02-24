50580665892a9ed7ad2158437856000b14396bba
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2024-02-24 11:54:28 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+#include <deque>
+#include<queue>
+#include<string>
+#include<stack>
+#include <tuple>
+#include <cmath>
+using namespace std;
+
+//vector<tuple <long long, int, int>> graph[1000001];
+//
+//int V, E;
+//
+//vector<int> parent(100005, -1);
+
+//int find(int x) {
+//    if (parent[x] < 0) return x;
+//    return parent[x] = find(parent[x]);
+//}
+//
+//bool is_diff_group(int u, int v) {
+//    u = find(u); v = find(v);
+//    if (u == v) return 0;
+//    if (parent[u] == parent[v])parent[u]--;
+//    if (parent[u] < parent[v]) parent[v] = u;
+//    else parent[u] = v;
+//    return 1;
+//}
+
+int N,K;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    int dp[1001] = { 0 };
+    int arr[1001] = { 0 };
+    
+    for (int i = 1; i <= N; i++) {
+        
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= i; j++) {
+            dp[i] = max(dp[i], dp[i - j] + arr[j]);
+        }
+    }
+
+    cout << dp[N];
+
+
+    return 0;
+
+
+}
