a811dfd9a6ebb5fcb05287c9486f2c1b2b225ad0
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2024-10-20 21:54:12 +0900

    [Gold V] Title: LCS, Time: 200 ms, Memory: 24868 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.js
+++ 백준/Gold/9251. LCS/LCS.js
@@ -1,18 +1,24 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n").map(String);
 
-const A = input[0];
-const B = input[1];
+let A = input[0];
+let B = input[1];
+let lenA = A.length;
+let lenB = B.length;
 
-let dp = Array.from({length : A.length + 1}, ()=>Array(B.length + 1).fill(0));
+A = " " + A; // 문자열 앞에 빈 칸을 추가하여 인덱스 1부터 시작하도록
+B = " " + B;
 
-for(let i = 1;i<= A.length;i++){
-    for(let j = 1;j<=B.length;j++){
-        if(A[i-1] === B[j-1]){
+let dp = Array.from({length : lenA+1}, () => Array(lenB+1).fill(0));
+
+// dp 배열을 이용하여 최장 공통 부분 수열 계산
+for (let i = 1; i <= lenA; i++) {
+    for (let j = 1; j <= lenB; j++) {
+        if (A[i] === B[j]) {
             dp[i][j] = dp[i-1][j-1] + 1;
-        }else{
+        } else {
             dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
         }
     }
 }
 
-console.log(dp[A.length][B.length]);
+console.log(dp[lenA][lenB]);  // 최종 답 출력
