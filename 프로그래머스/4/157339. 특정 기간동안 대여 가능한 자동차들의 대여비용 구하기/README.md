efd288614ade9f12729bf8f673ed3f0b9c8b6c07
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2024-11-08 23:45:07 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/README.md
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기 - 157339 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/157339#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/157339) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 02월 23일 13:28:23
+2024년 11월 08일 23:45:06
 
 ### 문제 설명
 
--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,13 +1,21 @@
-SELECT CAR_ID, C.CAR_TYPE, FLOOR(30*DAILY_FEE*(1-DISCOUNT_RATE/100)) AS FEE
-FROM CAR_RENTAL_COMPANY_CAR C
-JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN USING(CAR_TYPE)
-WHERE C.CAR_TYPE IN ('세단','SUV')
-AND CAR_ID NOT IN (SELECT CAR_ID
-                  FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-                  WHERE END_DATE >= '2022-11-1'
-                  AND START_DATE <= '2022-11-30')
-AND DURATION_TYPE = '30일 이상'
-HAVING FEE >= 500000 AND FEE < 2000000
-ORDER BY FEE DESC, CAR_TYPE ASC, CAR_ID DESC
+# CAR_RENTAL_COMPANY_CAR 테이블과 CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블과 CAR_RENTAL_COMPANY_DISCOUNT_PLAN 테이블에서 자동차 종류가 '세단' 또는 'SUV' 인 자동차 중 2022년 11월 1일부터 2022년 11월 30일까지 대여 가능하고 30일간의 대여 금액이 50만원 이상 200만원 미만인 자동차에 대해서 자동차 ID, 자동차 종류, 대여 금액(컬럼명: FEE) 리스트를 출력하는 SQL문을 작성해주세요. 결과는 대여 금액을 기준으로 내림차순 정렬하고, 대여 금액이 같은 경우 자동차 종류를 기준으로 오름차순 정렬, 자동차 종류까지 같은 경우 자동차 ID를 기준으로 내림차순 정렬해주세요.
 
-
+SELECT c.car_id, c.car_type,
+    CASE
+        WHEN c.car_type = 'SUV' THEN FLOOR(30 * c.daily_fee * (1 - p.discount_rate / 100))
+        WHEN c.car_type = '세단' THEN FLOOR(30 * c.daily_fee * (1 - p.discount_rate / 100))
+    END AS fee
+FROM car_rental_company_car c
+JOIN car_rental_company_discount_plan p 
+    ON c.car_type = p.car_type AND p.duration_type = '30일 이상'
+WHERE c.car_type IN ('SUV', '세단')
+    AND c.car_id NOT IN (
+        SELECT car_id
+        FROM car_rental_company_rental_history
+        WHERE start_date <= '2022-11-30' AND end_date >= '2022-11-01'
+    )
+    AND (
+        (c.car_type = 'SUV' AND FLOOR(30 * c.daily_fee * (1 - p.discount_rate / 100)) BETWEEN 500000 AND 2000000)
+        OR (c.car_type = '세단' AND FLOOR(30 * c.daily_fee * (1 - p.discount_rate / 100)) BETWEEN 500000 AND 2000000)
+    )
+ORDER BY fee DESC, c.car_type ASC, c.car_id DESC;
