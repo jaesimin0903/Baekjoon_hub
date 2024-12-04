ffe4b3fc083aab585e040d30f41f1886f5d67cdd
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2024-12-04 22:41:48 +0900

    [Gold IV] Title: 합이 0, Time: 1900 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.cc
+++ 백준/Gold/3151. 합이 0/합이 0.cc
@@ -1,39 +1,34 @@
 #include <iostream>
+#include <queue>
 #include <vector>
-#include <unordered_set>
-#include <unordered_map>
-#include <string>
 #include <algorithm>
 
 using namespace std;
 
-int arr[10010];
+int N;
+vector<int> v;
 
 int main() {
-    int N;
-    cin >> N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int n;
+		cin >> n;
+		v.push_back(n);
+	}
 
-    
-    unordered_map<int, int> num;
+	sort(v.begin(), v.end());
 
-    for (int i = 0; i < N; i++) {
-        cin >> arr[i];
-        num[arr[i]]++;
-    }
+	long long answer = 0;
 
-    sort(arr,arr+N);
+	for (int i = 0; i < N-1; i++) {
+		for (int j = i + 1; j < N; j++) {
+			auto l = lower_bound(v.begin() + j+1, v.end(), -(v[i] + v[j]));
+			auto r = upper_bound(v.begin() + j+1, v.end(), -(v[i] + v[j]));
+			answer += r - l;
+		}
+	}
 
-    long long ans = 0;
+	cout << answer;
 
-    for (int i = 0; i < N -1; i++) {
-        for (int j = i + 1; j < N; j++) {
-            auto ub = upper_bound(arr + j + 1, arr + N, -(arr[i] + arr[j]));
-            auto db = lower_bound(arr + j + 1, arr + N, -(arr[i] + arr[j]));
-            ans += ub - db;
-        }
-    }
-
-    cout << ans << "\n";
-
-    return 0;
+	return 0;
 }
